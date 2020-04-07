64774b13b5ed7a448f9f31ea2847cd4dd21200ad
Aleksandr GOrdeev <razamanaza@yandex.ru>
2020-04-07 02:20:26 +0000

    Added controllers and refactor to ES6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/home');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', homeController.index);
 
 module.exports = router;
