e18f6a50927af1699ddfe5cd3741d469321648f0
Aleksandr GOrdeev <razamanaza@yandex.ru>
2020-04-12 17:40:23 +1200

    Added webpack and moved reacts and sass to it

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
+/* app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
-}));
+})); */
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
