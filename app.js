8ad66104cd5e9c7ddeb7169562d337ab74b7253f
Aleksandr GOrdeev <razamanaza@yandex.ru>
2020-04-12 18:12:01 +1200

    Removed unneccessary routes and modules

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const aboutRouter = require('./routes/about');
 
 const app = express();
@@ -19,16 +17,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-/* app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-})); */
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
